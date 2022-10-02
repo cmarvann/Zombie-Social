fcf50b0774e7a8a58c8f2ad578fe2b1e46c3d216
cmarvann <covingtonmarvann@gmail.com>
2022-10-02 16:20:08 -0400

    add readme gitignore packagejson and models

---
+++ models/Reaction.js
@@ -0,0 +1,73 @@
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const ReplySchema = new Schema(
+  {
+    // set custom id to avoid confusion with parent comment's _id field
+    replyId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    replyBody: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    writtenBy: {
+      type: String,
+      required: true,
+      trim: true
+
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    }
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+);
+
+
+const ReactionSchema = new Schema({
+  writtenBy: {
+    type: String,
+    required: true
+  },
+  commentBody: {
+    type: String,
+    required: true,
+    maxlength: 280
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: createdAtVal => dateFormat(createdAtVal)
+  },
+  replies: [ReplySchema]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
+
+CommentSchema.virtual('replyCount').get(function() {
+  return this.replies.length;
+});
+
+
+const Comment = model('Reaction', ReactionSchema);
+
+
---
+++ models/User.js
@@ -0,0 +1,72 @@
+const { Model, DataTypes } = require('sequelize');
+
+
+
+
+// create fields/columns for User model
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+      required: true,
+      string: trim()
+    },
+    email: {
+      type: DataTypes.STRING,
+      required: true,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4]
+      }
+
+    },
+    thoughts: {
+      id: [],
+      ref: thought
+    },
+    friends: {
+      id: [],
+      ref: user
+    },
+  },
+  {
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      }
+  },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user'
+  }
+);
+user.virtual('friendCount').get(function () {
+  return this.thoughts.length;
+});
+module.exports = User;
+
